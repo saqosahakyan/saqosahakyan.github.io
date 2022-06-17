648870a984445107e0f38105d2ee49e49b849599
Sargis <sargis0099@mail.ru>
2022-06-17 21:05:49 +0400

    all project

--- index.js
+++ index.js
@@ -1,45 +1,4 @@
-
-// mutq sexmeluc bacvi modal login parol
-// mutqi koxq@ stexcmen knopka .grancvel anunov, vorin sexmeluc kbaci modal u kpahanji email, parol, u kkrnkel parol@
-// erb vor datark e dashter@ grancvelu ev mutq gorceluc heto tox patuhan bacvi ete parolner@ havasar exan tpi hajoxutyanb grancvel eq, kam ete havasar chen kama datak dasht ka tox tpi xndrunm em chisht lracreq
-// multfilmer sexmeluc mtni urish eji multeri bajin
-//iframe youtube texadrel im ejum  
-
-//local storage-ov grancvem, heto mutq linem kayq local storage-i mijocov
-//passwordi minimum length, uppercase, number
-
-//match, math, spread, splice, search
-
 /*
-let key;
-window.addEventListener("keydown", function (event) {
-    key = event.key;
-});
-
-const type = document.getElementsByClassName("typeValue");
-
-
-type[0].addEventListener("input", typeValue);
-
-function typeValue() {
-
-if(key === "a") {
-    type[0].style.backgroundColor = "blue";
-}
-    console.log(key);
-    
-  
-if(key === "b") {
-    type[0].style.backgroundColor = "green";
-}  
-    
-    console.log(type[0].value)
-}
-*/
-
-/*
-
-
 document.getElementById("next").addEventListener("click", function() {
     if(passOne.value === passTwo.value && passTwo.value !== "") {
         alert("Successful");
@@ -51,34 +10,6 @@ document.getElementById("next").addEventListener("click", function() {
     }
 })
 */
-/*
-document.getElementById("next").onclick = function(){
-    if(passOne.value === passTwo.value && passTwo.value !== "") {
-        alert("Successful");
-        document.getElementById("next"). onfocus = function() {
-            location.href= "https://www.facebook.com/"
-        }
-    } else {
-        alert("Try again")
-    }
-}
-*/
-
-// function verify() {
-//     console.log()
-//     var pass = passOne.value;
-//     var passw = passTwo.value;
-//     if (pass === passw && passw !== "") {
-//         console.log("Դուք հաջողությամբ գրանցվել եք");
-//         let saqo = document.getElementById("next");
-//         // document.getElementById("next").onfocus = function (){
-//             saqo.href = "https://www.facebook.com/"
-//         // }
-
-//     } else {
-//         alert("Սխալ գաղտնաբառ")
-//     
-// }
 
 const nextBtn = document.getElementById("next");
 const email = document.getElementById("email");
@@ -155,7 +86,6 @@ document.getElementById("delete").onclick = function(){
     log();
 }
 
-
 nextBtn.onclick = function() {
     var info = {
         logEmail: email.value,
@@ -164,8 +94,6 @@ nextBtn.onclick = function() {
     var str = JSON.stringify(info);
     localStorage.setItem("register", str);
 }
-
-
 
 const email66 = document.getElementById("email66");
 const pass66 = document.getElementById("pass66");
@@ -184,30 +112,15 @@ logIn.onclick = function() {
     }
 };
 
+function eyeOn(elemId) {
+    document.getElementById(elemId).type = "text"
+}
 
+function eyeOff(elemId) {
+    document.getElementById(elemId).type = "password"
+}
 
-
-
-
-
-
-//maail
-// document.getElementById("email0").addEventListener("focus", function changeColor() {
-//     document.getElementById("email0").style.backgroundColor="gray";
-// })
-
-// //search button
-// document.getElementById("stop").onmouseover = function() {
-//     // alert("Չի կարելի")
-// }
-
-// // element div
-// const addText = document.createElement("p");
-// addText.innerHTML = "Hello";
-// document.getElementById("newDiv").appendChild(addText).style.color = "green";
-
-// //button div
-// const btn = document.createElement("button");
-// btn.innerHTML = "Կոճակ";
-// document.getElementById("leftMenu").appendChild(btn)
+function backButton() {
+    document.getElementById("leftMenu").hidden = true
+}
 
